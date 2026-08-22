ae8c7ee596b03c0a5134d2f524cec786e9a125c8
Mabel Alamu <mabelalamu30@gmail.com>
2026-08-22 18:19:14 +0000

    Standardise tourney_name in int_matches_filtered

--- models/marts/intermediate/int_matches_filtered.sql
+++ models/marts/intermediate/int_matches_filtered.sql
@@ -5,11 +5,17 @@ with filtered as (
              when score like '%DEF%' then 'Default'
              when score like '%W/O%' then 'Walkover'
              else 'Completed'
-        end as match_status
+        end as match_status,
+        case
+            when tourney_name like 'Davis Cup%' then 'Davis Cup'
+            when tourney_name in ('ATP Tour Finals', 'ATP Finals', 'Tour Finals', 'Masters Cup') then 'ATP Finals'
+            when tourney_name in ('NextGen Finals', 'Next Gen Finals', 'Next Gen ATP Finals') then 'Next Gen Finals'
+            else tourney_name
+        end as standardised_tourney_name
     from {{ ref('int_matches_unioned') }} as imu
     where tourney_date >= '2000-01-01'
 )
--- select distinct score from filtered
--- where score not like '%-%'  -- filter out scores without a normal set-score pattern
+--  select score from filtered 
+-- where score not like '%-%'  -- getting a list of scores without a normal set-score pattern
 
 select * from filtered
