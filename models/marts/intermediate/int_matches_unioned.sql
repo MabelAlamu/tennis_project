b69b107bca5d622a144b73796aa7477507b4adda
Mabel Alamu <mabelalamu30@gmail.com>
2026-08-22 19:43:08 +0000

    Fix tourney_id collision between ATP and WTA in int_matches_unioned

--- models/marts/intermediate/int_matches_unioned.sql
+++ models/marts/intermediate/int_matches_unioned.sql
@@ -11,4 +11,7 @@ with combined as (
         'WTA' as tour
     from {{ ref('stg_wta') }} as wta
 )
-select * from combined
+select 
+    {{ dbt_utils.generate_surrogate_key(['tour', 'tourney_id']) }} as new_tourney_id,
+    c.* 
+from combined as c
--- models/marts/intermediate/int_matches_unpivoted.sql
+++ models/marts/intermediate/int_matches_unpivoted.sql
@@ -3,7 +3,7 @@ with unpivoted as (
     select
         match_id,
         tour,
-        tourney_id,
+        new_tourney_id as tourney_id,
         standardised_tourney_name as tourney_name,
         surface,
         draw_size,
@@ -43,7 +43,7 @@ with unpivoted as (
     select
         match_id,
         tour,
-        tourney_id,
+        new_tourney_id as tourney_id,
         standardised_tourney_name as tourney_name,
         surface,
         draw_size,
