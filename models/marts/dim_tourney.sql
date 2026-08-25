6d47eadec76c89e99213a0d69d550ab09f60bde1
Mabel Alamu <toluwalasealamu@gmail.com>
2026-08-25 18:12:21 +0000

    WIP: Pause dim_tourney work pending tournament ID fix

--- models/marts/dim_tourney.sql
+++ models/marts/dim_tourney.sql
@@ -2,6 +2,7 @@ with tourney_attrs as (
     select distinct 
         tourney_id,
         tourney_name,
+        tour,
         surface,
         draw_size,
         tourney_level, 
@@ -22,6 +23,7 @@ final as (
     select distinct 
         a.tourney_id,
         a.tourney_name,
+        a.tour,
         a.surface,
         a.draw_size,
         a.tourney_level, 
@@ -31,14 +33,6 @@ final as (
     from tourney_attrs as a
     left join champions as c
     on a.tourney_id = c.tourney_id 
-    and a.tourney_name = c.tourney_name
 )
+
 select * from final
-where tourney_id in
-    (select tourney_id from final
-    group by tourney_id
-    having count(distinct surface) > 1)
-order by tourney_id
-
-
---select * from final where tourney_id = '2010-D030'
