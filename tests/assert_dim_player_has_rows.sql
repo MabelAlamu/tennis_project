aa9e4e4241ea531e0fb57d10c274744527f40376
Mabel Alamu <alamumabel@gmail.com>
2026-08-29 03:18:30 +0100

    Verify CI protection with deliberate testing failure (#16)
    * Verify CI protection with deliberate testing failure: Validates that failed dbt CI checks prevent pull requests from being merged into the protected main branch.
    
    * Fix dim_player count test and verified that CI works as it should now
    
    ---------
    
    Co-authored-by: Mabel Alamu <toluwalasealamu@gmail.com>

---
+++ tests/assert_dim_player_has_rows.sql
@@ -0,0 +1,13 @@
+-- Deliberately incorrect for testing CI protection.
+-- A dbt test fails when this query returns one or more rows.
+
+with player_count as (
+
+    select count(*) as row_count
+    from {{ ref('dim_player') }}
+
+)
+
+select *
+from player_count
+where row_count = 0
